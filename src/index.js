acddcd9209df042c93a441e3b615165ecdd13551
cmsegui <cmsegui@yahoo.com>
2017-09-05 22:02:30 -0400

    tried to fix mistake

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/searchbar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/videolist';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyDjb25fwFljItTDNCZv-0j9cedC-rmqAJg';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+        this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
+}  
+
+ReactDOM.render(<App />, document.querySelector('.container'));
