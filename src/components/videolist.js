22256e0cee3650ba85841e5aab81f3f9be6c57ca
cmsegui <cmsegui@yahoo.com>
2017-09-07 15:57:03 -0400

    finished tutorial and everything is working

--- src/components/videolist.js
+++ src/components/videolist.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import VideoListItem from './components/videolistitem';
+import VideoListItem from './videolistitem';
 
 const VideoList = (props) => {
     const videoItems = props.videos.map((video) => {
-        return <VideoListItem key= {video.etag} video={video} />
+        return (
+            <VideoListItem
+              onVideoSelect={props.onVideoSelect}
+              key={video.etag}
+              video={video} />
+        )
     });
 
     return (
